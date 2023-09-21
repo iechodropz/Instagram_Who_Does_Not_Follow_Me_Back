7255fcd63d65a4ac07cc2966a16b01efdb8c7fbe
Brian Cerda <cerdabrian26@gmail.com>
2023-09-21 16:26:44 -0500

    Fixed bug.

--- initial_user_inputs.py
+++ initial_user_inputs.py
@@ -3,13 +3,11 @@ def initial_user_inputs():
 
     max_followers = ""
     while not max_followers.isdigit():
-        max_followers = int(
-            input(
-                "Please specify the maximum number of follower an unverified account can have: "
-            )
+        max_followers = input(
+            "Please specify the maximum number of follower an unverified account can have: "
         )
 
         if not max_followers.isdigit():
             print("Please enter a valid number for maximum number of followers.")
 
-    return username, max_followers
+    return username, int(max_followers)
